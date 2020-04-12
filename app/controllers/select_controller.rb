2ab43ba004c08683830fd5d760820b234a852a00
naotsukamoto <m0naaa0u@gmail.com>
2020-04-12 22:39:03 +0900

    ユーザ登録できるように修正

--- app/controllers/select_controller.rb
+++ app/controllers/select_controller.rb
@@ -1,5 +1,20 @@
 class SelectController < ApplicationController
   def top
     @teams = TeamMaster.all
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render '/logs/top'
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:login_id, :name, :mail_adress, :password, :favorite1)
+  end
+
 end
