3a61bff11f96235e37819b34a0a1f73ab2e006aa
naotsukamoto <m0naaa0u@gmail.com>
2020-04-15 23:47:56 +0900

    select_controller edited

--- app/controllers/select_controller.rb
+++ app/controllers/select_controller.rb
@@ -1,19 +1,25 @@
 class SelectController < ApplicationController
   def top
+    # TeamMasterインスタンスの取得
     @teams = TeamMaster.all
+    # 新規ユーザーのインスタンスを定義
     @user = User.new
   end
 
   def create
+    # 新規ユーザーのインスタンスの作成
     @user = User.new(user_params)
     if @user.save
+      # インスタンス変数にユーザーを保存できたら、/logs/topに遷移
       render '/logs/top'
     else
+      # 失敗したら、戻る
       render 'new'
     end
   end
 
   def user_params
+    # ユーザーインスタンスの生成に必須であるパラメータを定義
     params.require(:user).permit(:login_id, :name, :mail_adress, :password, :favorite1)
   end
 
