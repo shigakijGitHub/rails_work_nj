0a77a3cff6382fc790b7e11cc0adcb5452a085c5
shigakijGitHub <62395779+shigakijGitHub@users.noreply.github.com>
2020-05-01 15:54:43 +0900

    一旦コミット

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,11 +2,11 @@ class LogsController < ApplicationController
   def top
     # ユーザ情報取得
     @userInfo = User.find(params[:user_id])
-    
+
     # お気に入りを配列に詰める
     favoArry = [@userInfo.favorite1, @userInfo.favorite2, @userInfo.favorite3]
-    
-    # お気に入りがnullの場合、配列から削除
+
+    # お気に入りがnilの場合、配列から削除
     favoArry.delete_if {|n| n == nil }
 
     # チームマスタからお気に入り画像を取得
@@ -15,7 +15,23 @@ class LogsController < ApplicationController
         @logoArry << TeamMaster.find_by(team_id:favo).team_logo_url
     end
 
-    # お気に入りチームの全試合取得
+    # お気に入りチームの試合取得
+    @allGames = selectAllGames(favoArry)
+
+    # 観戦履歴取得
+    @userLogs = UsersLog.where(user_id:params[:user_id])
+
+    # 試合日程用の曜日リスト取得
+    @weeks = getWeeks()
+  end
+
+  # 試合日程用の曜日リスト返却
+  def getWeeks
+    ["月","火","水","木","金","土","日"]
+  end
+
+  # お気に入りチームの試合返却
+  def selectAllGames(favoArry)
     query = ActiveRecord::Base.send(
         :sanitize_sql_array,
         ['SELECT 
@@ -44,17 +60,10 @@ class LogsController < ApplicationController
          WHERE 
             agm.team_id_away IN (:favos)
             AND agm.team_id_home IN (:favos)
-         ORDER BY agm.game_date ASC 
-        ',favos: favoArry]
+         ORDER BY agm.game_date ASC',
+         favos: favoArry]
         )
-    @allGames = ActiveRecord::Base.connection.select_all(query)
-
-    # 全チーム取得
-
-    # 観戦履歴取得
-    @userLogs = UsersLog.where(user_id:params[:user_id])
-    # 試合日程用の曜日リスト
-    @weeks = ["月","火","水","木","金","土","日"]
-
+    allGames = ActiveRecord::Base.connection.select_all(query)
   end
+
 end
