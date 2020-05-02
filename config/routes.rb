8f423c51b5bc181583e65ebd625194cccbe17a5d
shigakijGitHub <62395779+shigakijGitHub@users.noreply.github.com>
2020-05-02 14:12:48 +0900

    routesに/logs/top追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'test_dir/test'
 
-  get 'logs/top'
+  get 'logs/top/:user_id' => "logs#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
