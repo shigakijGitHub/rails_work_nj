e53aef3703f8653435159d494e64d7fd5811cb7d
shigakijGitHub <62395779+shigakijGitHub@users.noreply.github.com>
2020-05-30 16:38:19 +0900

    エラー解消にためにコミット

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,7 +2,7 @@ class LogsController < ApplicationController
   # top画面表示
   def top
     # ユーザ情報取得
-    @userInfo = User.find(params[:user_id])
+    @userInfo = getUserInfo(params[:user_id])
 
     # お気に入りを配列に詰める
     favoList = [@userInfo.favorite1, @userInfo.favorite2, @userInfo.favorite3]
@@ -17,7 +17,7 @@ class LogsController < ApplicationController
     end
 
     # お気に入りチームの試合取得
-    @allGames = selectGames(favoList)
+    @allGames = selectAllGames(favoList)
 
     # 観戦履歴取得
     @userLogs = UsersLog.where(user_id:params[:user_id])
@@ -33,20 +33,27 @@ class LogsController < ApplicationController
 
   # detail画面表示
   def detail
+    # TODO game_idの改竄チェック
     # ユーザ情報取得
-    # game_idの改竄チェック
-    @game_id = params[:game_id]
+    @userInfo = getUserInfo(params[:user_id])
 
+    # 試合情報取得
+    @gameDetail = selectGameDetail(params[:game_id])
   end
 
-  
+
+  # ユーザ情報返却
+  def getUserInfo
+    User.find(params[:user_id])
+  end
+
   # 試合日程用の曜日リスト返却
   def getWeeks
     ["月","火","水","木","金","土","日"]
   end
 
   # お気に入りチームの試合返却
-  def selectGames(favoList)
+  def selectAllGames(favoList)
     query = ActiveRecord::Base.send(
         :sanitize_sql_array,
         ['SELECT 
@@ -76,7 +83,43 @@ class LogsController < ApplicationController
             ag.team_id_away IN (:favos)
             OR ag.team_id_home IN (:favos)
          ORDER BY ag.game_date ASC',
-         favos: favoList]
+         favos: favoList
+        ]
+        )
+    allGames = ActiveRecord::Base.connection.select_all(query)
+  end
+
+  # 選択された試合情報返却
+  def selectGameDetail(gameId)
+    query = ActiveRecord::Base.send(
+        :sanitize_sql_array,
+        ['SELECT 
+            gd.score_home    AS score_home,
+            gd.score_away    AS score_away,
+            ag.team_id_home  AS team_id_home,
+            ag.team_id_away  AS team_id_away,
+            ag.stadium_name  AS stadium_name,
+            ag.game_date     AS game_date,
+            gd.id            AS id,
+            (SELECT team_name
+             FROM all_teams 
+             WHERE id = ag.team_id_home) AS team_name_home,
+            (SELECT team_name
+             FROM all_teams 
+             WHERE id = ag.team_id_away) AS team_name_away,
+            (SELECT team_logo_url
+             FROM all_teams
+             WHERE id = ag.team_id_home) AS team_logo_url_home,
+            (SELECT team_logo_url
+             FROM all_teams 
+             WHERE id = ag.team_id_away) AS team_logo_url_away
+        FROM all_games ag
+            INNER JOIN game_details gd ON ag.id = gd.all_game_id
+            INNER JOIN all_teams at ON at.id = ag.team_id_home
+         WHERE 
+            ag.id = (:gameId)',
+         gameId: gameId
+        ]
         )
     allGames = ActiveRecord::Base.connection.select_all(query)
   end
